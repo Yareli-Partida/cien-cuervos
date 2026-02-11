15f34e0d1625bfe1a818380ef68d1ec010ed3e59
Yarel <yareli0528@gmail.com>
2026-02-10 19:15:29 -0800

    initial commit

---
+++ src/main/java/com/example/ciencuervos/main/CienCuervosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ciencuervos.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CienCuervosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CienCuervosApplication.class, args);
+    }
+
+}
